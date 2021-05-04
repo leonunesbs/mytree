327cd0b9a5cb2b510cb3e0e31d3153a6ce0e9f30
Leonardo Nunes <leonunesbs@gmail.com>
2021-05-04 18:31:45 -0300

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-33VJP2LVSZ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-33VJP2LVSZ');
+        </script>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;300;400;700&family=Oswald:wght@300;400;700&display=swap"
