43087e22ddcfe5703e7ffbb30365116d8f24ff85
leonunesbs <leonunesbs@gmail.com>
2021-05-03 23:13:27 -0300

    add favicon

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
+const withPWA = require("next-pwa");
+
 module.exports = {
   future: {
     webpack5: true,
   },
 };
+
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    cacheOnFrontEndNav: true,
+  },
+});
