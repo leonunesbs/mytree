ce0db0dc7dd97ca364d301a6bcbfb54ad7b3fe54
leonunesbs <leonunesbs@gmail.com>
2021-05-04 18:42:06 -0300

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import themeDefault from "@/styles/themeDefault";
+import { useRouter } from "next/router";
 import Head from "next/head";
+import { useEffect } from "react";
+import * as gtag from "../utils/gtag";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const handleRouteChange = (url: URL) => {
+        gtag.pageview(url);
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
+  }, [router.events]);
   return (
     <>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-33VJP2LVSZ"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-33VJP2LVSZ');
-        </script>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;300;400;700&family=Oswald:wght@300;400;700&display=swap"
