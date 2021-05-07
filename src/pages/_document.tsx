78e6424238c8e2cfa80f5b9aa15b11e203d9bc20
Leonardo Nunes <leonunesbs@gmail.com>
2021-05-06 21:13:26 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from "../utils/gtag";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
